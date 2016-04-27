3408722c5b9155f9af23fafb0e8109ce28ccfaaa
ILMARE <827574984@qq.com>
2016-04-27 17:12:14 +0800

    可以滑动删除的ListView

--- app/src/main/java/com/ilmare/swipedismisslistview/MainActivity.java
+++ app/src/main/java/com/ilmare/swipedismisslistview/MainActivity.java
@@ -50,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         swipeDismissListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
